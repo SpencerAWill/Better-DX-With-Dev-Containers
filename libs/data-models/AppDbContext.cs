ba4137e0e98c691dde0f778bc41b6f5f07595ca7
Spencer Will <spencer.a.will@gmail.com>
2026-04-08 20:34:50 +0000

    feat(devcontainers): add sidecar postgres database container for development with env configured
    Add a sidecar postgres container to the dev container compose configuration. Initialize two data
    library projects for EF Core and its migrations, respectively. Add VS Code tasks to assist with
    doing database stuff.

---
+++ libs/data-models/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Data.Models;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
